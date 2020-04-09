7af276be61e85bc9851811c6a61d92e7a3cd8671
DAntonenko <dmitry@dwpr.ru>
2020-04-09 20:37:20 +0300

    MVP

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('hello', function() {
-  console.log('Hello Zell');
-});
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
-  gulp.src('source-files')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('destination'))
+  return gulp.src('./src/*.+(scss|sass)')
+    .pipe(sass())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('autoprefixer', function () {
+  return gulp.src('dist/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist'));
 });
